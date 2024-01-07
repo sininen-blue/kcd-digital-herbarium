980786d10ea870e1a647c69c24e075483acbe35f
sininen-blue <alaanneil@gmail.com>
2024-01-07 18:13:35 +0800

    uhh

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module todo
+module main
 
 go 1.21.5
+
+require github.com/lib/pq v1.10.9
--- main.go
+++ main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+
+    _ "github.com/lib/pq"
 )
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("statis"))))
+    connStr := "postgres://postgres:secret@localhost:5432/gopgtest?sslmode=disable"
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        tmpl := template.Must(template.ParseFiles("./templates/index.html"))
-        tmpl.Execute(w, nil)
-    })
+    db, err := sql.Open("postgres", connStr)
+    defer db.Close()
 
-    http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-        tmpl := template.Must(
-            template.ParseFiles("./templates/fragments/results.html"),
-        )
-        data := map[string][]Stock {
-            "Results": SearchTicker(r.URL.Query().Get("key")),
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err = db.Ping(); err != nil {
+        log.Fatal(err)
     }
 
-        tmpl.Execute(w, data)
-    })
 
-    log.Println("App running on localhost:8000")
-    log.Fatal(http.ListenAndServe(":8000", nil))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("statis"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	log.Println("App running on localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
