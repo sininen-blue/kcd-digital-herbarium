a486ac5f3075fc5daf96b0666f2de4f2b4409248
sininen-blue <alaanneil@gmail.com>
2024-01-10 21:49:14 +0800

    it kinda works for now

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module main
 go 1.21.5
 
 require github.com/lib/pq v1.10.9
+
+require github.com/mattn/go-sqlite3 v1.14.19 // indirect
--- main.go
+++ main.go
@@ -6,22 +6,21 @@ import (
 	"log"
 	"net/http"
 
-    _ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+
+type Ingredient struct {
+    Name string
+    Description string
+}
+
 func main() {
-    connStr := "postgres://postgres:secret@localhost:5432/gopgtest?sslmode=disable"
-
-    db, err := sql.Open("postgres", connStr)
-    defer db.Close()
-
+    db, err := sql.Open("sqlite3", "./db/herbarium")
     if err != nil {
         log.Fatal(err)
     }
-    if err = db.Ping(); err != nil {
-        log.Fatal(err)
-    }
-
+    defer db.Close()
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("statis"))))
 
@@ -31,6 +30,34 @@ func main() {
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.ParseFiles("./templates/fragments/results.html"))
+
+        query_string := "select name, description from ingredients where name like ?"
+        query_key := "%"+r.URL.Query().Get("key")+"%"
+
+        rows, err := db.Query(query_string, query_key)
+        if err != nil {
+            log.Println("first")
+            log.Fatal(err)
+        }
+        defer rows.Close()
+
+        var results []Ingredient
+        for rows.Next() {
+            var name string
+            var description string
+            err = rows.Scan(&name, &description)
+            if err != nil {
+                log.Fatal(err)
+            }
+            ingredient := Ingredient{Name: name, Description: description}
+            results = append(results, ingredient)
+        }
+
+        data := map[string][]Ingredient {
+            "results": results,
+        }
+        tmpl.Execute(w, data)
 
 	})
 
