8bf2a8d819c992df065aa7d99de045944d3d7ff0
sininen-blue <alaanneil@gmail.com>
2024-02-03 18:27:22 +0800

    added air and partial and inventory

--- src/main.go
+++ src/main.go
@@ -27,6 +27,12 @@ type Ingredient struct {
 	url         string
 }
 
+var currentInv []invItem
+type invItem struct {
+    Ingredient Ingredient
+    Amount int
+}
+
 func getAllIngredients() []Ingredient {
     var results []Ingredient
 
@@ -53,6 +59,7 @@ func getAllIngredients() []Ingredient {
     return results
 }
 
+// not done
 func getIngredients(query string, key string) []Ingredient{
     var results []Ingredient
 
@@ -63,6 +70,22 @@ func getIngredients(query string, key string) []Ingredient{
     defer rows.Close()
 
     return results
+}
+
+func getIngredient(query string, key string) Ingredient{
+    var result Ingredient
+
+    row := db.QueryRow(query, key)
+    err := row.Scan(
+        &result.Id,
+        &result.Name,
+        &result.Description,
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return result
 }
 
 func getPotions(query string) []Potion {
@@ -97,7 +120,8 @@ func main() {
     defer db.Close()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/", addInventory).Methods("POST")
 	http.Handle("/", r)
 
 	log.Println("App running on localhost:8000")
@@ -108,7 +132,20 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
     data := map[string]interface{} {
         "Title": "Index",
         "Ingredients": getAllIngredients(),
+        "Inventory": currentInv,
     }
 
     templ.ExecuteTemplate(w, "base", data)
+}
+
+func addInventory(w http.ResponseWriter, r *http.Request) {
+    key := r.FormValue("ingredient")
+
+    query := "select name from ingredients where name == ?"
+    ing := getIngredient(query, key)
+    item := invItem{Ingredient: ing, Amount: 1} 
+
+    currentInv = append(currentInv, item)
+
+    templ.ExecuteTemplate(w, "invItem", item)
 }
