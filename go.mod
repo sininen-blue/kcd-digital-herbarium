67dbeac44d9c798387e304c4ce4ac37a7328a19c
sininen-blue <alaanneil@gmail.com>
2024-02-03 14:46:45 +0800

    BACK TO TEMPLATES BABY

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/sininen-blue/kcd-digital-herbarium
 go 1.21.6
 
 require (
-	github.com/a-h/templ v0.2.543
 	github.com/gorilla/mux v1.8.1
+	github.com/maragudk/gomponents v0.20.1
 	github.com/mattn/go-sqlite3 v1.14.20
 )
--- src/main.go
+++ src/main.go
@@ -1,48 +1,66 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 
-    "github.com/a-h/templ"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var templ *template.Template
+
+type Page struct {
+    Title string
+}
+
 type Potion struct {
-    Name string
-    Description string
+	Name        string
+	Description string
+	Effects     string
+	url         string
+}
+
+func getPotions(query string) []Potion {
+	var results []Potion
+
+	return results
+}
+
+func init() {
+	var err error
+
+	funcMap := template.FuncMap{
+		"inc": func(i int) int { return i + 1 },
+        "dec": func(i int) int { return i - 1 },
+	}
+
+    templ = template.New("template")
+    templ = templ.Funcs(funcMap)
+	templ, err = templ.ParseGlob("./templates/*.html")
+	if err != nil {
+		log.Println("template error")
+		log.Fatal(err)
+	}
+
 }
 
 func main() {
 	r := mux.NewRouter()
-    r.HandleFunc("/", indexHandler)
-    r.HandleFunc("/potions/", potionHandler)
+	r.HandleFunc("/", indexHandler)
 	http.Handle("/", r)
-
 
 	log.Println("App running on localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    // TODO replace with db query
-    var potionSlice []Potion
-    pot := Potion{Name: "fuck", Description: "it makes you fuck"}
-    potionSlice = append(potionSlice, pot)
+    page := Page{Title: "test"}
 
-    query := r.URL.Query().Get("q")
-    if query != "" {
+    data := map[string]interface{} {
+        "Page": page,
     }
 
-        templ.Handler(home(potionSlice).ServeHTTP(w, r)
-}
-
-
-func potionHandler(w http.ResponseWriter, r *http.Request) {
-    var potionSlice []Potion
-    pot := Potion{Name: "fuck", Description: "it makes you fuck"}
-    potionSlice = append(potionSlice, pot)
-
-    templ.Handler(potions(potionSlice)).ServeHTTP(w, r)
+    templ.ExecuteTemplate(w, "base", data)
 }
