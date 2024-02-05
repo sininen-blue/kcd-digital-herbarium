cc829f94cc2975f7f507fa271c3fae764e4ea131
sininen-blue <alaanneil@gmail.com>
2024-02-05 16:35:23 +0800

    chores: format

--- src/main.go
+++ src/main.go
@@ -21,187 +21,185 @@ type Potion struct {
 }
 
 type Ingredient struct {
-    Id string
-    Name string
-    Description string
+	Id          string
+	Name        string
+	Description string
 	url         string
 }
 
 type invItem struct {
-    Id string
-    IngredientName string
-    Amount int
+	Id             string
+	IngredientName string
+	Amount         int
 }
 
 func (i invItem) Save() {
-    tx, err := db.Begin()
-    if err != nil {
-        log.Fatal(err)
-    }
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    stmt, err := tx.Prepare("insert into inventory(ingredientName, amount) values(?, ?)")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer stmt.Close()
+	stmt, err := tx.Prepare("insert into inventory(ingredientName, amount) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
 
-    _, err = stmt.Exec(i.IngredientName, i.Amount)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = stmt.Exec(i.IngredientName, i.Amount)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = tx.Commit()
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-
 func (i invItem) Update() {
-    tx, err := db.Begin()
-    if err != nil {
-        log.Fatal(err)
-    }
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    stmt, err := tx.Prepare("update inventory set ingredientName = ?, amount = ? where rowid = ?")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer stmt.Close()
+	stmt, err := tx.Prepare("update inventory set ingredientName = ?, amount = ? where rowid = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
 
-    _, err = stmt.Exec(i.IngredientName, i.Amount, i.Id)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = stmt.Exec(i.IngredientName, i.Amount, i.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = tx.Commit()
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func (i invItem) Delete() {
-    tx, err := db.Begin()
-    if err != nil {
-        log.Fatal(err)
-    }
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    stmt, err := tx.Prepare("delete from inventory where rowid = ?")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer stmt.Close()
+	stmt, err := tx.Prepare("delete from inventory where rowid = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
 
-    _, err = stmt.Exec(i.Id)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err = stmt.Exec(i.Id)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = tx.Commit()
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func (i invItem) Inc() {
-    i.Amount += 1
-    i.Update()
+	i.Amount += 1
+	i.Update()
 }
 
 func (i invItem) Dec() {
-    i.Amount -= 1
+	i.Amount -= 1
 
-    if i.Amount <= 0 {
-        i.Delete()
-    } else {
-        i.Update()
-    }
+	if i.Amount <= 0 {
+		i.Delete()
+	} else {
+		i.Update()
+	}
 }
 
 func getAllIngredients() []Ingredient {
-    var results []Ingredient
+	var results []Ingredient
 
-    query := "select * from ingredients"
-    rows, err := db.Query(query)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
+	query := "select * from ingredients"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var i Ingredient
-        err = rows.Scan(
-            &i.Id,
-            &i.Name,
-            &i.Description,
-        )
-        if err != nil {
-            log.Fatal(err)
-        }
-        results = append(results, i)
-    }
+	for rows.Next() {
+		var i Ingredient
+		err = rows.Scan(
+			&i.Id,
+			&i.Name,
+			&i.Description,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, i)
+	}
 
-    return results
+	return results
 }
 
-func getIngredient(query string, key string) Ingredient{
-    var result Ingredient
+func getIngredient(query string, key string) Ingredient {
+	var result Ingredient
 
-    row := db.QueryRow(query, key)
-    err := row.Scan(
-        &result.Id,
-        &result.Name,
-        &result.Description,
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
+	row := db.QueryRow(query, key)
+	err := row.Scan(
+		&result.Id,
+		&result.Name,
+		&result.Description,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return result
+	return result
 }
 
 func getAllInventory() []invItem {
-    var results []invItem
+	var results []invItem
 
-    query := "select * from inventory"
-    rows, err := db.Query(query)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
+	query := "select * from inventory"
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var i invItem
-        err = rows.Scan(
-            &i.Id,
-            &i.IngredientName,
-            &i.Amount,
-        )
-        if err != nil {
-            log.Fatal(err)
-        }
-        results = append(results, i)
-    }
+	for rows.Next() {
+		var i invItem
+		err = rows.Scan(
+			&i.Id,
+			&i.IngredientName,
+			&i.Amount,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, i)
+	}
 
-    return results
+	return results
 }
 
-func getInventory(query string, key string) invItem{
-    var result invItem
+func getInventory(query string, key string) invItem {
+	var result invItem
 
-    row := db.QueryRow(query, key)
-    err := row.Scan(
-        &result.Id,
-        &result.IngredientName,
-        &result.Amount,
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
+	row := db.QueryRow(query, key)
+	err := row.Scan(
+		&result.Id,
+		&result.IngredientName,
+		&result.Amount,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return result
+	return result
 }
-
 
 func getPotions(query string) []Potion {
 	var results []Potion
@@ -214,35 +212,35 @@ func init() {
 
 	funcMap := template.FuncMap{
 		"inc": func(i int) int { return i + 1 },
-        "dec": func(i int) int { return i - 1 },
+		"dec": func(i int) int { return i - 1 },
 	}
 
-    templ = template.New("template")
-    templ = templ.Funcs(funcMap)
+	templ = template.New("template")
+	templ = templ.Funcs(funcMap)
 	templ, err = templ.ParseGlob("./templates/*.html")
 	if err != nil {
 		log.Println("template error")
 		log.Fatal(err)
 	}
 
-    db, err = sql.Open("sqlite3", "./db/herbarium.db")
-    if err != nil {
-        log.Fatal(err)
-    }
+	db, err = sql.Open("sqlite3", "./db/herbarium.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-    defer db.Close()
+	defer db.Close()
 
 	r := mux.NewRouter()
-    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	r.HandleFunc("/", indexHandler).Methods("GET")
 	r.HandleFunc("/", addInventory).Methods("POST")
 	r.HandleFunc("/{ingredient}/add", itemAdd).Methods("POST")
 	r.HandleFunc("/{ingredient}/subtract", itemSubtract).Methods("POST")
 
-    r.HandleFunc("/potions/", potionHandler).Methods("GET")
+	r.HandleFunc("/potions/", potionHandler).Methods("GET")
 	http.Handle("/", r)
 
 	log.Println("App running on localhost:8000")
@@ -250,60 +248,60 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    data := map[string]interface{} {
-        "Title": "Index",
-        "Ingredients": getAllIngredients(),
-        "Inventory": getAllInventory(),
-    }
+	data := map[string]interface{}{
+		"Title":       "Index",
+		"Ingredients": getAllIngredients(),
+		"Inventory":   getAllInventory(),
+	}
 
-    templ.ExecuteTemplate(w, "base", data)
+	templ.ExecuteTemplate(w, "base", data)
 }
 
 func addInventory(w http.ResponseWriter, r *http.Request) {
-    // still need true validation
-    query := "select * from ingredients where name like ?"
-    key := "%"+r.FormValue("ingredient")+"%"
-    ing := getIngredient(query, key)
+	// still need true validation
+	query := "select * from ingredients where name like ?"
+	key := "%" + r.FormValue("ingredient") + "%"
+	ing := getIngredient(query, key)
 
-    exists := false
-    for _, invItem := range getAllInventory() {
-        if invItem.IngredientName == ing.Name {
-            exists = true
-            invItem.Inc()
-        }
-    }
+	exists := false
+	for _, invItem := range getAllInventory() {
+		if invItem.IngredientName == ing.Name {
+			exists = true
+			invItem.Inc()
+		}
+	}
 
-    if exists == false {
-        item := invItem{IngredientName: ing.Name, Amount: 1} 
-        item.Save()
-        templ.ExecuteTemplate(w, "invItem", item)
-    } else {
-        w.Header().Add("hx-trigger", "changedInv")
-    }
+	if exists == false {
+		item := invItem{IngredientName: ing.Name, Amount: 1}
+		item.Save()
+		templ.ExecuteTemplate(w, "invItem", item)
+	} else {
+		w.Header().Add("hx-trigger", "changedInv")
+	}
 }
 
 func itemAdd(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
+	vars := mux.Vars(r)
 
-    for _, item := range getAllInventory() {
-        if item.IngredientName == vars["ingredient"] {
-            item.Inc()
-        }
-    }
+	for _, item := range getAllInventory() {
+		if item.IngredientName == vars["ingredient"] {
+			item.Inc()
+		}
+	}
 
-    w.Header().Add("hx-trigger", "changedInv")
+	w.Header().Add("hx-trigger", "changedInv")
 }
 
 func itemSubtract(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
+	vars := mux.Vars(r)
 
-    for _, item := range getAllInventory() {
-        if item.IngredientName == vars["ingredient"] {
-            item.Dec()
-        }
-    }
+	for _, item := range getAllInventory() {
+		if item.IngredientName == vars["ingredient"] {
+			item.Dec()
+		}
+	}
 
-    w.Header().Add("hx-trigger", "changedInv")
+	w.Header().Add("hx-trigger", "changedInv")
 }
 
 func potionHandler(w http.ResponseWriter, r *http.Request) {
